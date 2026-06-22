118b7dccfcc150b02787b4109d252fac1b9eb011
Arian <arian.kulmer@web.de>
2026-06-22 18:27:43 +0200

    Revert "FP6: Switch to libperfmgr"
    This reverts commit 98039eb16bf5170158b950013a3fac2a0c3a524d.
    
    Revert "FP6: Import qcom perf boosts into powerhint.json"
    This reverts commit 178f1cefcfeb5b5ad548ae8c9a3a5706a77fa822.
    
    Revert "FP6: power: Update powerhint.json"
    This reverts commit 8b5991dc569d359fb1fcb70f6545bc42ce1e85df.
    
    Change-Id: I7780cb69cea502001f15b08e57c514c0f2c4a386

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -202,7 +202,6 @@ BOOT_SECURITY_PATCH := 2026-05-05
 VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 
 # Sepolicy
-include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
