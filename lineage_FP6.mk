30cbdc207ed2806aae0dc49b7033a2dc45fb286d
Arian <arian.kulmer@web.de>
2026-06-28 15:02:03 +0200

    FP6: Update to FP6.QREL.16.82.0
    Change-Id: Ifce6981f910f6636e24f13fa23e6900456ff5a39

--- lineage_FP6.mk
+++ lineage_FP6.mk
@@ -22,8 +22,8 @@ PRODUCT_NAME_FOR_ATTESTATION := FP6
 PRODUCT_GMS_CLIENTID_BASE := android-fairphone
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="qssi_64-user 16 FP6.QREL.16.69.0 WS1B release-keys" \
-    BuildFingerprint=Fairphone/FP6/FP6:16/FP6.QREL.16.69.0/WS1B:user/release-keys \
+    BuildDesc="qssi_64-user 16 FP6.QREL.16.82.0 WS1F release-keys" \
+    BuildFingerprint=Fairphone/FP6/FP6:16/FP6.QREL.16.82.0/WS1F:user/release-keys \
     DeviceName=FP6 \
     DeviceProduct=FP6 \
     SystemDevice=FP6 \
